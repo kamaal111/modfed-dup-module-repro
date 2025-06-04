e1c5a93d00711ab4291769f6b2b09eb541c0cc0b
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-04 18:10:36 +0000

    Loading as typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const webPackConfig = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
