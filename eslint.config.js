cfd59e692105e0666f946496c730cf26b1b642fa
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-06 08:28:21 +0000

    Fixing quality checks

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,15 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  globalIgnores(['apps/*/node_modules/**', 'apps/*/dist/**']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
